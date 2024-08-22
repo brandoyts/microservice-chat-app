54c461198f1ef4a385ece9a562cba51341d2392b
brandoyts <brando.endona.dev@gmail.com>
2024-08-22 11:45:00 +0800

    initial commit

--- socket/src/index.ts
+++ socket/src/index.ts
@@ -1,5 +1,8 @@
-import * as express from 'express'
+import dotenv from 'dotenv'
+import express from 'express'
 import { createServer } from 'http'
+
+dotenv.config()
 
 const app = express()
 const server = createServer(app)
